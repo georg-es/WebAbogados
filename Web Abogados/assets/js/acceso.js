8155a2d9ccd44e31a08a30473ec8d507e221bcaf
georg-es <jorgefelo2005@gmail.com>
2025-08-31 22:07:19 -0500

    Delete Web Abogados directory

--- Web Abogados/assets/js/acceso.js
+++
@@ -1,5 +0,0 @@
-const usuario = JSON.parse(localStorage.getItem("usuario"));
-if (!usuario) {
-  alert("Debes iniciar sesión para acceder a esta sección.");
-  window.location.href = "login.html";
-}
